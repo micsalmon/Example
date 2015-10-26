47bb15dda7478a0645b446a2c8ec366702793ae4
msalmon14 <msalmon14@ITIC-042-9020M.eil-server.cba.ua.edu>
2015-10-26 15:49:54 -0500

    Commit five

--- Example/src/Example.java
+++ Example/src/Example.java
@@ -3,7 +3,7 @@ public class Example {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Georgia Little League");
+		System.out.println("Hello GLL");
 	}
 
 }
